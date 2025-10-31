18cc741ba3425cad0a2933301ef94b15c4abc7ce
2593186 <ss1488ua@gmail.com>
2025-10-31 12:41:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+from datetime import datetime
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+users = {}
+
+@app.route('/')
+def index():
+    return render_template('chat.html')
+
+@socketio.on('connect')
+def connect():
+    print('Клиент подключился:', request.sid)
+
+@socketio.on('disconnect')
+def disconnect():
+    if request.sid in users:
+        username = users.pop(request.sid)
+        emit('message', {'user': 'Система', 'text': f'{username} вышел из чата'}, broadcast=True)
+    print('Клиент отключился:', request.sid)
+
+@socketio.on('join')
+def join(data):
+    username = data.get('username', 'Аноним')
+    users[request.sid] = username
+    emit('message', {'user': 'Система', 'text': f'{username} вошёл в чат'}, broadcast=True)
+
+@socketio.on('chat_message')
+def chat_message(data):
+    username = users.get(request.sid, 'Аноним')
+    text = data.get('text', '')
+    time = datetime.now().strftime('%H:%M')
+    emit('message', {'user': username, 'text': text, 'time': time}, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
